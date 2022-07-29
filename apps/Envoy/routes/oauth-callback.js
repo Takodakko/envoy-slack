63fdfbf20ba110b2624e6be5373ff966e1a128ad
Graham Pierce <grahamp@envoy.com>
2022-07-29 11:31:59 -0400

    experimenting

--- apps/Envoy/routes/oauth-callback.js
+++ apps/Envoy/routes/oauth-callback.js
@@ -86,7 +86,8 @@ const _requestAccessAndRefreshTokens = async (code) => {
 	});
 
 	let options = {
-		url: `${process.env.ENVOY_AUTH_URL}/oauth2/token`,
+		// url: `${process.env.ENVOY_AUTH_URL}/oauth2/token`,
+        url: `${process.env.ENVOY_BASE_URL}/a/auth/v0/token`,
 		method: "POST",
 		headers: headers,
 		body: dataString,
